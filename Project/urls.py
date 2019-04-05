cf4ef67460ebade0edfbe7ba248ae81e61dd182b
UMULISA12 <umulisaa0@gmail.com>
2019-04-05 12:49:26 +0200

    INSTALLED APPS

--- Project/urls.py
+++ Project/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     url(r'',include('projectspost.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^logout/$', views.logout, {"next_page": '/'}),
+    
     url(r'^api-token-auth/', obtain_auth_token)
     
 ]
